1e4127e88fe5cb018a94950504af4f9b33388726
IshaRathod28 <124258565+IshaRathod28@users.noreply.github.com>
2023-02-28 07:31:47 +0530

    Create EmpData.js

---
+++ EmpData.js
@@ -0,0 +1,56 @@
+export const employee_data = [
+    {
+        id:"1",
+        FirstName:"Isha",
+        LastName:"Rathod",
+        Email:"dgs@gmail.com",
+        PhoneNumber:"6675475678",
+        Gender:"female",
+        Department:"Full Stack",
+        Skills:"Communication",
+        About:"hieyfwguq"
+    },{
+        id:"2",
+        FirstName:"Maulik",
+        LastName:"Prajapati",
+        Email:"mgs@gmail.com",
+        PhoneNumber:"8675475678",
+        Gender:"male",
+        Department:"Full Stack",
+        Skills:"Programming",
+        About:"hieyfwguqsf"
+    },
+    {
+        id:"3",
+        FirstName:"Prince",
+        LastName:"Kumbhani",
+        Email:"kgs@gmail.com",
+        PhoneNumber:"1675475678",
+        Gender:"male",
+        Department:"Node JS",
+        Skills:"Backend Technology",
+        About:"hieyfwguqvdsgf"
+    },
+    {
+        id:"4",
+        FirstName:"Adnan",
+        LastName:"Samol",
+        Email:"ags@gmail.com",
+        PhoneNumber:"2675475678",
+        Gender:"male",
+        Department:"Full Stack",
+        Skills:"Programming",
+        About:"hieyfwgudfq"
+    },
+    {
+        id:"5",
+        FirstName:"Shubham",
+        LastName:"Talati",
+        Email:"sgs@gmail.com",
+        PhoneNumber:"4675475678",
+        Gender:"male",
+        Department:"Full Stack",
+        Skills:"Optimization",
+        About:"hieyfwguqhgfwe"
+    }
+]
